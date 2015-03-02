c2b81b5f510cd7f57f202118aff2869ede6f2e91
Kristin Skjørseter <Kristinhoel@hotmail.com>
2015-03-02 15:31:23 +0100

    new task

---
+++ src/TextReverse.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class TextReverse {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Skriv inn en string:");
+		String stringInput = input.nextLine();
+
+	}
+
+}
